5d9e0516237b46ea63414036d06f5369c84e7630
alexa0503 <lori.w@live.cn>
2015-10-28 11:14:12 +0800

    修复

--- src/AppBundle/EventListener/OAuthListener.php
+++ src/AppBundle/EventListener/OAuthListener.php
@@ -65,7 +65,7 @@ class OAuthListener
 			$session->set('wx_nonce_str', $wx->nonceStr);
 			$session->set('wx_signature', $wx->signature);
 			$session->set('wx_share_url', $request->getUriForPath('/'));
-			$session->set('wx_title', '搭出不一"Young"');
+			$session->set('wx_title', '搭出不一\"Young\"');
 			$session->set('wx_desc', '年轻的Show场，由你搭配不一样的时尚');
 			$session->set('wx_img_url', '/bundles/app/default/m/images/share.jpg');
 		}
